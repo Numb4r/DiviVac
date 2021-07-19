07e2064502fe696f5731460de0b38aaaaf94c2d5
Numb4r <yuridimitre@protonmail.com>
2021-07-19 15:05:25 -0300

    corrigido script para apenas executar sed em html files

--- md2html.sh
+++ md2html.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [[ ! -d 'out' ]] &&  mkdir out
-ls |grep .md| cut -d . -f 1  | xargs -I {} pandoc -f markdown {}.md  -o out/{}.html
-ls out/ | xargs -I {} sed -i "1 i <head><link rel='stylesheet' href='style.css'></head>" out/{}
+ls |grep .md| cut -d . -f 1  | xargs -t -I {} pandoc -f markdown {}.md  -o out/{}.html
+ls out/ | grep .html| xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='style.css'></head>" out/{}
