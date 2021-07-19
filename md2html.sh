060f3e9f678e96c5d1000160c996f9e8aa061437
Numb4r <yuridimitre@protonmail.com>
2021-07-19 15:12:03 -0300

    script agora substitui todas as mencoes a md por html

--- md2html.sh
+++ md2html.sh
@@ -2,3 +2,4 @@
 [[ ! -d 'out' ]] &&  mkdir out
 ls |grep .md| cut -d . -f 1  | xargs -t -I {} pandoc -f markdown {}.md  -o out/{}.html
 ls out/ | grep .html| xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='style.css'></head>" out/{}
+ls out/ | grep .html | xargs -I {} sed -i "s/.md/.html/g" out/{}
