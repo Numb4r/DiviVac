8b2199420f100424837b06aaa82f4ab92c552083
Numb4r <yuridimitre@protonmail.com>
2021-07-21 10:11:51 -0300

    Update script de conversao

--- md2html.sh
+++ md2html.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-[[ ! -d 'out' ]] &&  mkdir out
-ls |grep .md| cut -d . -f 1  | xargs -t -I {} pandoc -f markdown {}.md  -o out/{}.html
-ls out/ | grep .html| xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='style.css'></head>" out/{}
-ls out/ | grep .html | xargs -I {} sed -i "s/.md/.html/g" out/{}
+target='Markdown'
+folder='Main'
+html="${folder}/html"
+css="${folder}/css"
+img="${folder}/img"
+[[ ! -d $folder ]] &&  mkdir $folder
+[[ ! -d $html ]] && mkdir $html
+[[ ! -d $css ]] && mkdir $css
+[[ ! -d $img ]] && mkdir $img
+ls $target | grep .md | cut -d . -f 1 | xargs -t -I {} pandoc -f markdown $target/{}.md  -o $html/{}.html
+ls $html/ | grep .html| xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='style.css'></head>" $html/{}
+ls $html/ | grep .html | xargs -I {} sed -i "s/.md/.html/g" $html/{}
