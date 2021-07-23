50b50be88bf67eadff46ba86eda1eb42535bd40f
Numb4r <yuridimitre@protonmail.com>
2021-07-23 17:29:10 -0300

    script corrigido

--- md2html.sh
+++ md2html.sh
@@ -8,9 +8,9 @@ img="${folder}/img"
 [[ ! -d $html ]] && mkdir $html
 [[ ! -d $css ]] && mkdir $css
 [[ ! -d $img ]] && mkdir $img
-ls $target | grep .md | cut -d . -f 1 | xargs -t -I {} pandoc -f markdown $target/{}.md  -o $html/{}.html
-ls $html/ | grep .html| xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='../css/style.css'></head>" $html/{}
+ls $target | grep .md  | cut -d . -f 1 | xargs -t -I {} pandoc -f markdown $target/{}.md  -o $html/{}.html
+ls $html/ | grep .html | xargs  -I {} sed -i "1 i <head><link rel='stylesheet' href='../css/style.css'></head>" $html/{}
 ls $html/ | grep .html | xargs -I {} sed -i "s/.md/.html/g" $html/{}
 ls $html/ | grep .html | xargs -I {} sed -i "s/doc\//img\//g" $html/{}
 cp doc/*.css $css
-cp doc/*.jpg $img
+cp doc/*.{jpg,jpeg} $img
